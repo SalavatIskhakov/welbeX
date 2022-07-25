6537514453935532a5b7abef407fafd23af97543
NatGames <salavat-iskhakov@mail.ru>
2022-07-25 15:25:49 +0300

    use react for dropdown

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,29 +1,49 @@
+import { useEffect, useState } from 'react';
 import './app-filter.css';
 
 const AppFilter = (props) => {
   const { filter, condition, onFilterSelect, onConditionSelect } = props;
+  const [columnClass, serColumnClass] = useState('dropdown-content');
+  const [conditionClass, setConditionClass] = useState('dropdown-content');
 
-  function myFunction(className) {
-    document.getElementById(className).classList.toggle("show");
+  function dropdownClick(className, setClassName) {
+    let buf = className.split(' ');
+    serColumnClass('dropdown-content');
+    setConditionClass('dropdown-content');
+    if (buf.length === 1) {
+      setClassName(`${className} show`);
+    }
   }
+
+  useEffect(() => {
+
+  }, [columnClass, conditionClass]);
 
   const buttons =
     <div className="buttons">
-      <div className="dropdown">
-        <button onClick={() => myFunction("column")} className="dropbtn">{filter}</button>
-        <div id="column" className="dropdown-content">
-          <button onClick={() => onFilterSelect('name')}>name</button>
-          <button onClick={() => onFilterSelect('quantity')}>quantity</button>
-          <button onClick={() => onFilterSelect('distance')}>distance</button>
+      <div className="dropdown" onBlur={() => setTimeout(() => dropdownClick(columnClass, serColumnClass), 0)}>
+        <button
+          className="dropbtn"
+          onClick={() => dropdownClick(columnClass, serColumnClass)}
+        >{filter}
+        </button>
+        <div className={columnClass}>
+          <button onClick={() => { onFilterSelect('name'); dropdownClick(columnClass, serColumnClass); }}>name</button>
+          <button onClick={() => { onFilterSelect('quantity'); dropdownClick(columnClass, serColumnClass); }}>quantity</button>
+          <button onClick={() => { onFilterSelect('distance'); dropdownClick(columnClass, serColumnClass); }}>distance</button>
         </div>
       </div>
-      <div className="dropdown">
-        <button onClick={() => myFunction("condition")} className="dropbtn">{condition}</button>
-        <div id="condition" className="dropdown-content">
-          <button onClick={() => onConditionSelect('include')}>include</button>
-          <button onClick={() => onConditionSelect('equals')}>equals</button>
-          <button onClick={() => onConditionSelect('more')}>more</button>
-          <button onClick={() => onConditionSelect('less')}>less</button>
+      <div className="dropdown" onBlur={() => setTimeout(() => dropdownClick(conditionClass, setConditionClass), 0)}>
+        <button
+          className="dropbtn"
+          onClick={() => { dropdownClick(conditionClass, setConditionClass) }}
+        >{condition}
+        </button>
+        <div className={conditionClass}>
+          <button onClick={() => { onConditionSelect('include'); dropdownClick(conditionClass, setConditionClass); }}>include</button>
+          <button onClick={() => { onConditionSelect('equals'); dropdownClick(conditionClass, setConditionClass); }}>equals</button>
+          <button onClick={() => { onConditionSelect('more'); dropdownClick(conditionClass, setConditionClass); }}>more</button>
+          <button onClick={() => { onConditionSelect('less'); dropdownClick(conditionClass, setConditionClass); }}>less</button>
         </div>
       </div>
     </div>
